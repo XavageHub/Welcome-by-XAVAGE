1b155081a3bb351fd85a62078c044e7b201ba581
XavageHub <91792532+XavageHub@users.noreply.github.com>
2021-10-04 00:24:50 +0700

    Update main.py

--- welcome bot/main.py
+++ welcome bot/main.py
@@ -14,9 +14,10 @@ async def on_member_join(member):
   guild = member.guild
   channel = bot.get_channel(int(cid))
   if channel is not None:
-    embed = discord.Embed(title=f"ยินดีต้อนรับสู่เซิร์ฟเวอร์ **{guild.name}**\nขอให้อยู่ด้วยกันนานๆนะ", description=f"", color=discord.Color.from_rgb(153, 102, 255))
+    embed = discord.Embed(title=f"ยินดีต้อนรับสู่แก๊งค์ **{guild.name}**\nขอให้อยู่ด้วยกันนานๆนะครับ", description=f"", color=discord.Color.from_rgb(177, 10, 255))
     embed.set_author(name = f"{member.name}#{member.discriminator}", icon_url = member.avatar_url)
-    embed.set_image(url="https://i.pinimg.com/originals/06/80/81/068081ee5b913a47003a64f7233825fe.gif")
+    embed.set_image(url="https://share.creavite.co/uNRJZUSr40ECnMvd.gif")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/893177764461547542/893339738512166932/standard.gif")
     try:
       await channel.send(embed=embed)
       print(f"<@!{member.id}> has joined the server")
@@ -25,21 +26,6 @@ async def on_member_join(member):
   else:
       print("Failed to fetch channel")
       
-@bot.event
-async def on_member_remove(member):
-  guild = member.guild
-  channel = bot.get_channel(int(cid))
-  if channel is not None:
-    embed = discord.Embed(title=f"ได้ออกจากเซิร์ฟเวอร์ **{guild.name}** แล้ว\nหวังว่าจะได้มาพบกันอีกนะ", description=f"", color=discord.Color.from_rgb(153, 102, 255))
-    embed.set_author(name = f"{member.name}#{member.discriminator}", icon_url = member.avatar_url)
-    embed.set_image(url="https://i.imgur.com/ogeTrhI.gif")
-    try:
-      await channel.send(embed=embed)
-      print(f"<@!{member.id}> has left the server")
-    except:
-      print("i can't do that")
-  else:
-      print("Failed to fetch channel")
 
 token = os.environ.get("TOKEN")
 bot.run(token)
